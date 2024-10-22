6138da0cfb93d4f75b812ae37164b99c755f3407
Kiyohiro Kambayashi <k.kambayashi@westernsydney.edu.au>
2024-10-22 11:23:28 +1100

    Modify file name

--- bin/widget-cdk.ts
+++ bin/widget-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { WidgetCdkStack } from '../lib/widget_cdk-stack';
+import { WidgetCdkStack } from '../lib/widget-cdk-stack';
 
 const app = new cdk.App();
 new WidgetCdkStack(app, 'WidgetCdkStack', {
